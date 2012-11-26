0b356cbaef6f7acd9e1a48638aa18d07763e20a6
Fabian Schneider <fabian.schneider@bit-care.de>
2012-11-26 18:43:15 +0100

    Funktioniert zumindest mit dem Ciphertext aus der Übung

--- blatt4/crack_vigenere.c
+++ blatt4/crack_vigenere.c
@@ -57,16 +57,44 @@ int key_length(const char* cipher){
 
     return i;
 }
+
+char encrypted_E(double p[]){
+	int i,m=0;
+	for (i=0; i<FREQSIZE;i++){
+		if (p[i]>p[m])
+			m=i;
+	}
+	return 'A' + m ;
+}
+
+void find_key(char cipher[], char key[]){
+	int i, k, l;
+	double p[FREQSIZE];
+	l=strlen(cipher);
+	k = key_length(cipher);
+	for (i = 0; i < k; i++){
+		clear_p(p);
+		freq_analysis(p,cipher,l,k,i);
+		key[i] = (((encrypted_E(p) - 'A') - 4 + 26) % 26) + 'A';
+	}
+	key[i]='\0';
+}
+
+
+
 int main(void){
-    char cipher[PSIZE];
-    int i;
+	char cipher[PSIZE], key[PSIZE], plain[PSIZE];
 
     printf("Please enter ciphertext:\n");
     readLine(cipher);
     printf("\n");
 
-    i= key_length(cipher);
-    printf("Länge: %d\n",i);
+	find_key(cipher,key);
+	strncpy(plain,cipher,PSIZE);
+	decrypt(plain,key);
+
+	printf("Key: \t\t%s\n",key);
+	printf("Plaintext: \t%s\n",plain);
 
     return 0;
 }
