7e1441b0d2c58a6d79709397d6119d86c815e0e5
Fabian Schneider <fabian.schneider@bit-care.de>
2012-12-07 16:32:53 +0100

    Gedanken.

--- blatt7/my_alloc.c
+++ blatt7/my_alloc.c
@@ -9,9 +9,14 @@ struct header{
 };
 typedef struct header header;
 
-header root = {0,0};
+// Wurzelelement 
+header *root;
+// Aktuelles Element des Rings gemäß circular first fit
 header *node;
 
+typedef long align;
+
+/*
 bool is_allocated(header *memarea){
     if (memarea == &root)
         return false;
@@ -27,6 +32,7 @@ bool is_allocated(header *memarea){
 header *successor(header *memarea){
     return (header *) ((char *)(memarea+1)+memarea->size);
 }
+*/
 
 void init_my_alloc() {
     root.next = &root;
@@ -36,11 +42,16 @@ void init_my_alloc() {
 void* my_alloc(size_t size) {
     if (size <= 0)
         exit(1);
-    //Align check
-    
+
+    // Richtet angeforderte Größe auf durch 8 teilbare Gößen aus
+    // Sollte eigentlich laut Aufgabenstellung nicht notwendig sein
+    if (size % sizeof(align))
+        size += sizeof(align) - size % sizeof(align);
+
     header *prev = node; 
     header *current = prev->next;
 
+    // Suche ausreichend große freie Fläche
     do {
         if (current->size >= size)
             break;
@@ -48,8 +59,32 @@ void* my_alloc(size_t size) {
         current = current->next;
     } while (current != node);
 
-    if (current->size < size)
-        return 0;
+    // Situation:
+    // Entweder Block gefunden, dann current->size >= size
+    // oder eben nicht ;)
+
+    if (current->size < size){
+        // Kein ausreichend großer Block da.
+        // Neuen Block anforder
+        // Header erstellen
+        // in den Ring freier Flächen einsortieren
+        // current auf den neuen Header setzen
+    }
+
+    // Situation:
+    // current zeigt jetzt aufjedenfall auf ausreichend großen freien Block
+
+    // Entweder ist der Block genau gleich groß, dann kann er nach Korrektur der Zeiger
+    // direkt zurück gegeben werden.
+    //
+    // oder:
+    //
+    // Der Block ist zu groß, dann muss er von hinten verkleinert werden
+    // -> Neuer Header new_H erstellen. 
+    // -> header *new_header = (header *)((char *) current + current->size - size;
+    // -> current->size -= size + sizeof(header);
+    // -> return new_header + 1;
+
 
     if (current->size < size + 2 * sizeof(header)){
         node = current->next;
@@ -72,8 +107,6 @@ void* my_alloc(size_t size) {
     newnode->next = current;
     
     header *next = successor(current);
-
-    // Irgendwas mit dynmem
 
     current->size -= size + sizeof(header);
     return (void *)(newnode + 1);
