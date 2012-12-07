656688b8b10481a1969cf3cdb0eacc03ab480a9f
Fabian Schneider <fabian.schneider@bit-care.de>
2012-12-07 10:20:35 +0100

    Weiter das skript kopiert.

--- blatt7/my_alloc.c
+++ blatt7/my_alloc.c
@@ -10,6 +10,7 @@ struct header{
 typedef struct header header;
 
 header root = {0,0};
+header *node;
 
 bool is_allocated(header *memarea){
     if (memarea == &root)
@@ -29,10 +30,53 @@ header *successor(header *memarea){
 
 void init_my_alloc() {
     root.next = &root;
+    node = &root;
 }
 
 void* my_alloc(size_t size) {
-    return get_block_from_system();
+    if (size <= 0)
+        exit(1);
+    //Align check
+    
+    header *prev = node; 
+    header *current = prev->next;
+
+    do {
+        if (current->size >= size)
+            break;
+        prev = current;
+        current = current->next;
+    } while (current != node);
+
+    if (current->size < size)
+        return 0;
+
+    if (current->size < size + 2 * sizeof(header)){
+        node = current->next;
+        prev->next = current->next;
+
+        for (header *p = prev; p<current; p=successor(p)){
+            prev = p;
+        }
+        current->next = prev;
+
+        if (current == node)
+            node = &root;
+        return (void *) (current+1);
+    }
+    node = current;
+
+    header *newnode = (header *)((char *)current + current->size - size);
+    
+    newnode->size = size;
+    newnode->next = current;
+    
+    header *next = successor(current);
+
+    // Irgendwas mit dynmem
+
+    current->size -= size + sizeof(header);
+    return (void *)(newnode + 1);
 }
 
 void my_free(void* ptr) {
